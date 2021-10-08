a79f8557f6af1984b2e09893dcb3d3b450e832e0
Colin Mason <43448644+thechadinator@users.noreply.github.com>
2021-10-08 00:28:50 -0400

    Adding comments for further clarity

--- Client/Pages/TodoItem.cs
+++ Client/Pages/TodoItem.cs
@@ -1,5 +1,6 @@
 ﻿public class TodoItem
 {
+    //creating a constructor of sorts for the items in a todo list, setting a title and displying a check for the event if the action is marked as done   
     public string Title { get; set; }
     public bool IsDone { get; set; }
 }
